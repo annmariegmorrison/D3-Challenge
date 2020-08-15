e0cbd532605d7d969c0273602701ab09fa40fe40
AnnMarie Morrison <annmariemorrison@annmaries-mbp.home>
2020-08-14 20:33:52 -0500

    update

--- D3_Data_Journalism/assets/js/app.js
+++ D3_Data_Journalism/assets/js/app.js
@@ -23,7 +23,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv").then(function(data){
+d3.csv("assets/data/data.csv").then(function(data)
     console.log(data)
 
         // Step 1: Parse Data/Cast as numbers
