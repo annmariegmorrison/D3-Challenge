9fde6c9df4ae6038db9ccb82f8b774d811f968ca
AnnMarie Morrison <annmariemorrison@annmaries-mbp.home>
2020-08-14 19:33:54 -0500

    update

---
+++ D3_Data_Journalism/assets/js/app.js
@@ -0,0 +1,125 @@
+// @TODO: YOUR CODE HERE!
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(data){
+    console.log(data)
+
+
+        // Step 1: Parse Data/Cast as numbers
+        data.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.healthcareLow = +data.healthcareLow;
+            data.abbr = data.abbr;
+        });
+        
+        // Step 2: Create scale functions
+
+        var xLinearScale = d3.scaleLinear()
+            .domain([30, d3.max(Data, d => d.age)])
+            .range([0, width]);
+  
+        var yLinearScale = d3.scaleLinear()
+            .domain([8, d3.max(Data, d => d.smokes)])
+            .range([height, 0]);
+
+        // Step 3: Create axis functions
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+
+        // Step 4: Append axes to the chart
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+  
+        chartGroup.append("g")
+            .call(leftAxis);
+
+        // Step 5: Create circles
+        var circlesGroup = chartGroup.selectAll("circle")
+        .data(Data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", "10")
+        .attr("fill", "#98ceeb")
+        // .attr("opacity", ".5");
+    
+        var text = chartGroup.selectAll()
+        .data(Data)
+        .enter()
+        .append("text")
+        .attr("dx", d => xLinearScale(d.age))
+        .attr("dy", d => yLinearScale(d.smokes))
+        .text(d => (d.abbr))
+        .attr("fill", "white")
+        .attr("font-size", 8)
+        .style("text-anchor", "middle");
+
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Smokes (%)")
+        .attr("font-weight", 900)
+        .attr("font-size", 20);
+  
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Age(Median)")
+        .attr("font-weight", 900)
+        .attr("font-size", 20);
+    }).catch(function(error) {
+        console.log(error);
+    });
+
+
+    // Initialize Tooltip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<strong>${dateFormatter(d.date)}<strong><hr>${d.medals}
+            medal(s) won`);
+    });
+
+  
+    // Create tooltip in the chartGroup
+    chartGroup.call(toolTip);
+  
+    chartGroup.on("click", function(data) {
+    toolTip.show(data, this);
+  })
+
+    // Create mouseout event
+    .on("mouseout", function(d) {
+        toolTip.hide(d);
+    });
+
